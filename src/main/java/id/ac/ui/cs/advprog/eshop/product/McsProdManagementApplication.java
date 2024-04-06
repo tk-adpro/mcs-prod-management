463e9a82ef1fde0439ecb7a02396bffa5b7dbe92
Anisa Maharani <anismyes@gmail.com>
2024-04-06 15:48:56 +0800

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/McsProdManagementApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsProdManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsProdManagementApplication.class, args);
+    }
+
+}
