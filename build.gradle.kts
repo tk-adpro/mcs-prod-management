d8dca8c7b15a27d6e793470c10341ea365921489
bryanjeshua <bryanjmt04@gmail.com>
2024-04-17 16:35:13 +0700

    First try deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+    jacoco
 }
 
 group = "id.ac.ui.cs.advprog.eshop"
 version = "0.0.1-SNAPSHOT"
-
+java.sourceCompatibility = JavaVersion.VERSION_17
 java {
     sourceCompatibility = JavaVersion.VERSION_21
 }
@@ -42,4 +43,20 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests
+    run
+}
+tasks.jacocoTestReport {
+classDirectories.setFrom(files(classDirectories.files.map {
+fileTree(it) { exclude("**/*Application**") }
+}))
+dependsOn(tasks.test) // tests are required to run before generating the report
+reports {
+xml.required.set(false)
+csv.required.set(false)
+html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+}
 }
