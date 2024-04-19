5e0667bc0ffaa2f1f4d79e891c37987d76d11317
bryanjeshua <bryanjmt04@gmail.com>
2024-04-19 11:02:32 +0700

    fix application.properties

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/McsProdManagementApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/McsProdManagementApplication.java
@@ -2,9 +2,10 @@ package id.ac.ui.cs.advprog.eshop.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class McsProdManagementApplication {
 
     public static void main(String[] args) {
