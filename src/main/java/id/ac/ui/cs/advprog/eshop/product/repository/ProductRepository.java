12d62abec8483769b7990750d5523879317a2eb8
bryanjeshua <bryanjmt04@gmail.com>
2024-04-23 14:11:42 +0700

    [GREEN] Implement ProductRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/repository/ProductRepository.java
@@ -0,0 +1,59 @@
+package id.ac.ui.cs.advprog.eshop.product.repository;
+
+import id.ac.ui.cs.advprog.eshop.product.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductRepository {
+    private List<Product> productData = new ArrayList<>();
+
+    public Product create(Product product) {
+        if(product == null){
+            return null;
+        }
+        if (product.getProductId() == null || product.getProductId().isEmpty()) {
+            product.setProductId(java.util.UUID.randomUUID().toString());
+        }
+        productData.add(product);
+        return product;
+    }
+
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
+
+    public Product findById(String productId) {
+        if (productId == null || productId.isEmpty()) {
+            return null;
+        }
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product update(String id, Product updatedProduct) {
+        if (id == null || id.isEmpty() || updatedProduct == null) {
+            return null;
+        }
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(id)) {
+                updatedProduct.setProductId(id);  
+                productData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;  
+    }
+
+    public boolean delete(String productId) {
+        return productData.removeIf(product -> product.getProductId().equals(productId));
+    }
+}
