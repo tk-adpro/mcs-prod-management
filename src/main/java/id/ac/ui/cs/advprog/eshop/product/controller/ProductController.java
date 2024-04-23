138cad699d00039becf93d6ffe48d0492e341947
bryanjeshua <bryanjmt04@gmail.com>
2024-04-23 21:11:59 +0700

    [GREEN] Implement ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/controller/ProductController.java
@@ -1,13 +1,54 @@
 package id.ac.ui.cs.advprog.eshop.product.controller;
 
+import id.ac.ui.cs.advprog.eshop.product.*;
+import id.ac.ui.cs.advprog.eshop.product.model.Product;
+import id.ac.ui.cs.advprog.eshop.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
-    @GetMapping("/")
-    public ResponseEntity<String> getHello() {
-        return ResponseEntity.ok("Hello product!");
-    };
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/getAllProducts")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = service.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/getProductById/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable String productId) {
+        Product product = service.findById(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/createProduct")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = service.create(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/updateProduct/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable String productId, @RequestBody Product product) {
+        Product updatedProduct = service.update(product);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteProduct/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable String productId) {
+        boolean deleted = service.delete(productId);
+        if (deleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
