2d21edad4f2322b1ea0f26c3962e7414cf51a3b3
bryanjeshua <bryanjmt04@gmail.com>
2024-05-01 11:17:30 +0700

    [REFACTOR] Turn Notification.java into JPA-able

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/model/Notification.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/model/Notification.java
@@ -2,11 +2,31 @@ package id.ac.ui.cs.advprog.eshop.product.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "Notification")
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
     private String notificationId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
+
+    @Column(name = "is_read")
     private boolean isRead;
 }
