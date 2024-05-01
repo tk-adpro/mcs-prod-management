3a8949d6288327c10eacb707e1384504051067b6
bryanjeshua <bryanjmt04@gmail.com>
2024-05-01 11:25:34 +0700

    [GREEN] Refactor NotificationService.java JPA-able

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/service/NotificationService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/service/NotificationService.java
@@ -3,10 +3,11 @@ package id.ac.ui.cs.advprog.eshop.product.service;
 import id.ac.ui.cs.advprog.eshop.product.model.Notification;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationService {
     Notification create(Notification notification);
     List<Notification> findAll();
-    Notification findById(String id);
+    Optional<Notification> findById(String id);
     Notification update(String id, Notification updatedNotification);
 }
