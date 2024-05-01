10fe990948ea5e723d27ed3559ea2bbd35415e37
bryanjeshua <bryanjmt04@gmail.com>
2024-05-01 11:26:04 +0700

    [GREEN] Refactor ProductService.java JPA-able

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/service/ProductService.java
@@ -2,12 +2,12 @@ package id.ac.ui.cs.advprog.eshop.product.service;
 
 import id.ac.ui.cs.advprog.eshop.product.model.Product;
 import java.util.List;
-
+import java.util.Optional;
 public interface ProductService {
     void setSortStrategy(SortStrategy sortStrategy);
     Product create(Product product);
     List<Product> findAll();
-    Product findById(String productId);
+    Optional<Product> findById(String productId);
     boolean delete(String productId);
     Product update(Product product);
 }
