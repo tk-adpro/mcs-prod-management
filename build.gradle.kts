39222735a0b27a65f6ba46a1cac2dbdb69a1ca54
bryanjeshua <bryanjmt04@gmail.com>
2024-05-01 18:20:42 +0700

    chores : build.gradle.kts setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,13 +75,23 @@ tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 tasks.jacocoTestReport {
-classDirectories.setFrom(files(classDirectories.files.map {
-fileTree(it) { exclude("**/*Application**") }
-}))
-dependsOn(tasks.test) // tests are required to run before generating the report
-reports {
-xml.required.set(false)
-csv.required.set(false)
-html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-}
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) { 
+                    exclude("**/*Application**") 
+                    }
+            }
+        )
+    )
+
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(
+            layout.buildDirectory.dir("jacocoHtml")
+        )
+    }
 }
