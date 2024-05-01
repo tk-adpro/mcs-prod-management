649960127d4a022cc0260937b7836063f471d299
bryanjeshua <bryanjmt04@gmail.com>
2024-05-01 12:19:57 +0700

    [GREEN] Refactor NotificationController.java REST

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/controller/NotificationController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/controller/NotificationController.java
@@ -3,47 +3,45 @@ package id.ac.ui.cs.advprog.eshop.product.controller;
 import id.ac.ui.cs.advprog.eshop.product.model.Notification;
 import id.ac.ui.cs.advprog.eshop.product.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import org.springframework.http.HttpStatus;
 
-@Controller
+@RestController
 @RequestMapping("/notification")
 public class NotificationController {
 
     @Autowired
     private NotificationService service;
 
-    @PutMapping("/updateNotification/{notificationId}")
-    public ResponseEntity<Notification> updateNotification(@PathVariable String notificationId, @RequestBody Notification notification) {
-        Notification updatedNotification = service.update(notificationId, notification);
-        return new ResponseEntity<>(updatedNotification, HttpStatus.OK);
-    }
-
     @PostMapping("/createNotification")
     public ResponseEntity<Notification> createNotification(@RequestBody Notification notification) {
         Notification createdNotification = service.create(notification);
-        return new ResponseEntity<>(createdNotification, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/getNotificationById/{notificationId}")
-    public ResponseEntity<Notification> getNotificationById(@PathVariable String notificationId) {
-        Optional<Notification> notification = service.findById(notificationId);
-        if(notification.isPresent()){
-            return ResponseEntity.ok(notification.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdNotification);
     }
 
     @GetMapping("/getAllNotification")
     public ResponseEntity<List<Notification>> getAllNotification() {
         List<Notification> notifications = service.findAll();
-        return new ResponseEntity<>(notifications, HttpStatus.OK);
+        return ResponseEntity.ok(notifications);
     }
 
+    @GetMapping("/getNotificationById/{notificationId}")
+    public ResponseEntity<Notification> getNotificationById(@PathVariable String notificationId) {
+        return service.findById(notificationId)
+                      .map(ResponseEntity::ok) // If found, return 200 OK with notification
+                      .orElseGet(() -> ResponseEntity.notFound().build()); // If not found, return 404 Not Found
+    }
+
+    @PutMapping("/updateNotification/{notificationId}")
+    public ResponseEntity<Notification> updateNotification(@PathVariable String notificationId, @RequestBody Notification notification) {
+        return service.findById(notificationId)
+                    .map(n -> {
+                        Notification updatedNotification = service.update(notificationId, notification);
+                        return ResponseEntity.ok(updatedNotification);
+                    }) 
+                      .orElseGet(() -> ResponseEntity.notFound().build()); // If not found, return 404 Not Found
+    }
 }
