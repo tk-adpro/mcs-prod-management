24305e3c619463cc051495c4c9b8dd1ed6ff326b
bryanjeshua <bryanjmt04@gmail.com>
2024-05-15 17:53:30 +0700

    add image for profiler

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Code Coverage Test
 ![](image/ss1.png)
+
+# Grafana/Promotheus Monitoring
+![](image/ss2.png)
+
+
+# Profiling
+![](image/ss3.png)
+![](image/ss4.png)
+
--- src/main/java/id/ac/ui/cs/advprog/eshop/product/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/config/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
                         .requestMatchers("/**/admin/**").hasRole("ADMIN")
                         .requestMatchers("/**/customer/**").hasRole("CUSTOMER")
                         .requestMatchers("/**/user/**").hasRole("USER")
-                        .anyRequest().authenticated()
+                        .requestMatchers("/actuator/**").permitAll() 
+                        .anyRequest().authenticated()            
                 ).addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
