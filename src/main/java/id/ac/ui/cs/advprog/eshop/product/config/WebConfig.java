99c5168cadc0c1eaf65f9c6da84372142f292919
bryanjeshua <bryanjmt04@gmail.com>
2024-05-19 22:21:09 +0700

    setup config

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
@@ -1,19 +1,24 @@
 package id.ac.ui.cs.advprog.eshop.product.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ *
+ * @author Anisa.Maharani
+ * @author Bryan.Jeshua
+ *
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Adjust this to be more restrictive depending on your security needs
-            .allowedOrigins("http://localhost:8081")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
