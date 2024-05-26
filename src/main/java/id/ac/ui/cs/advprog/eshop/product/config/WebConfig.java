df2aadfc0e7b013a01305c36eba29d6501342980
bryanjeshua <bryanjmt04@gmail.com>
2024-05-26 16:55:44 +0700

    remove cors allowance

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
@@ -15,7 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
