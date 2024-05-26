85bc134cb6ba0c963dc0ccea7f6f221ed9de1e21
bryanjeshua <bryanjmt04@gmail.com>
2024-05-26 17:01:00 +0700

    fix cors

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
