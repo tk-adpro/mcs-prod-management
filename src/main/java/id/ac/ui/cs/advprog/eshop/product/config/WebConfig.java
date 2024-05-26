4e60499667470a2af8c8062b943025a12ecfcd9f
bryanjeshua <bryanjmt04@gmail.com>
2024-05-26 17:29:50 +0700

    fix config

--- src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/product/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "http://localhost:8080", "http://35.198.209.120")
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:8080", "http://35.198.209.120", "http://34.126.65.178")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
